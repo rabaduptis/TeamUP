d076f64cfb22f3e8e22303a2e42cd0c0e44c344b
root-14 <ilkaygavaz2@gmail.com>
2023-11-11 13:18:19 +0300

    Initial commit

---
+++ app/src/main/java/com/root14/teamup/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.root14.teamup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
