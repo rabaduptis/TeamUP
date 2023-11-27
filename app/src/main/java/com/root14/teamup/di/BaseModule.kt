26c790ed64052f95d4eaa0590c196d2fb171bab9
root-14 <ilkaygavaz2@gmail.com>
2023-11-27 22:04:01 +0300

    fix ExceptionHiltModules
    adding package info top of viewmodel

--- app/src/main/java/com/root14/teamup/di/BaseModule.kt
+++ app/src/main/java/com/root14/teamup/di/BaseModule.kt
@@ -6,15 +6,16 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@Module
 @InstallIn(SingletonComponent::class)
+@Module
 object BaseModule {
     @Singleton
     @Provides
-    fun providePrefDataStoreManager(@ActivityContext context: Context): PrefDataStoreManager {
+    fun providePrefDataStoreManager(@ApplicationContext context: Context): PrefDataStoreManager {
         return PrefDataStoreManager.getInstance(context)
     }
 
--- app/src/main/java/com/root14/teamup/viewmodel/CreateTeamViewModel.kt
+++ app/src/main/java/com/root14/teamup/viewmodel/CreateTeamViewModel.kt
@@ -1,3 +1,5 @@
+package com.root14.teamup.viewmodel
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
